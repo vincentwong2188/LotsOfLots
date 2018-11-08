83e577e67c9fced8da255fa38de760712e0ef927
millasml <millasml@yahoo.com.sg>
2018-11-08 19:15:17 +0800

    added home page

--- app/src/main/java/com/g2/androidapp/lotsoflots/HomePage.java
+++ app/src/main/java/com/g2/androidapp/lotsoflots/HomePage.java
@@ -1,46 +1,66 @@
 package com.g2.androidapp.lotsoflots;
-
+// autocomplete.getPlace().geometry.location --> https://stackoverflow.com/questions/10957649/google-maps-autocomplete-how-to-get-lat-lng
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
 
+    String location;
+    String addressName;
+    String[] Addresses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Button SearchBtn = (Button) findViewById(R.id.SearchBtn);
-        final Intent intent = new Intent(this, FirstInput.class);
-        final Intent toMaps = new Intent(this, MapsActivity.class);
-
+        final Intent intent = new Intent(this, TestAutoComplete.class);
         SearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(toMaps);
+                // goes to Shenc's code
             }
         });
-
+        Button RefreshBtn = (Button) findViewById(R.id.RefreshBtn);
+        RefreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bookmarkList();
+            }
+        });
         Button AddBookmark = (Button) findViewById(R.id.AddBkmk);
         AddBookmark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(intent);
+
+                startActivityForResult(intent , 1014);
+
+
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode , int resultCode , Intent data){
+        requestCode = 1014; resultCode = RESULT_OK;
+        location = data.getStringExtra("location");
+        addressName = data.getStringExtra("addressName") ;
+        Log.i("location", location);
+        Log.i("addressName" , addressName);
+
+    }
+    public void bookmarkList() {
         LinearLayout scrollContents = findViewById(R.id.scrollContents);
-
-        for(int i = 1; i <= 20 ; i++){
+        for (int i = 1; i <=1; i++) {
             LinearLayout itemLayout = new LinearLayout(this);
 
             RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
@@ -48,20 +68,18 @@ public class HomePage extends AppCompatActivity {
                     RelativeLayout.LayoutParams.WRAP_CONTENT
             );
 
-            lp.setMargins(10,10,10,10);
+            lp.setMargins(10, 10, 10, 10);
             itemLayout.setLayoutParams(lp);
             itemLayout.setOrientation(LinearLayout.VERTICAL);
 
-            TextView title = new TextView(this);
-            TextView contents = new TextView(this);
-            title.setText("Title " + i);
+            Button title = new Button(this);
+            //SharedPreferences bkmkAdd = PreferenceManager.getDefaultSharedPreferences(this);
+            //String addressBkmk = bkmkAdd.getString("Address", "Not available");
+            title.setText(addressName);
             title.setTypeface(title.getTypeface(), Typeface.BOLD_ITALIC);
             title.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
             title.setPadding(5, 5, 5, 5);
-            contents.setText("my text " + i);
-            contents.setPadding(5, 5, 5, 20);
             itemLayout.addView(title);
-            itemLayout.addView(contents);
 
             View v = new View(this);
             v.setLayoutParams(new LinearLayout.LayoutParams(
@@ -73,4 +91,7 @@ public class HomePage extends AppCompatActivity {
             scrollContents.addView(itemLayout);
         }
     }
+
+
+
 }
