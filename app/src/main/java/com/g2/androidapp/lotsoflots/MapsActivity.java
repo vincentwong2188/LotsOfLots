8343823b1407592802672d8da4d0679898540ad8
Shen Chen <shenc.96@gmail.com>
2018-11-10 21:47:31 +0800

    map changes

--- app/src/main/java/com/g2/androidapp/lotsoflots/MapsActivity.java
+++ app/src/main/java/com/g2/androidapp/lotsoflots/MapsActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.location.Location;
+import android.os.Parcel;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.ActivityCompat;
@@ -23,20 +24,28 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
+import com.google.android.gms.location.places.AutocompletePredictionBufferResponse;
+import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
     private BottomSheetBehavior mBottomSheetBehavior;
+    private GeoDataClient mGeoDataClient;
+    private SearchSuggestion mSearchSuggestion;
+    private ArrayList<CarPark> listToDisplay = new ArrayList<>(0);
 
     final int LOCATION_PERMISSION_REQUEST_CODE = 21;
 
@@ -45,7 +54,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        FloatingSearchView mSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
+        final FloatingSearchView mSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
 
         mSearchView.setOnMenuItemClickListener(new FloatingSearchView.OnMenuItemClickListener() {
             @Override
@@ -61,6 +70,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+
+
         mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
             @Override
             public void onSearchTextChanged(String oldQuery, final String newQuery) {
@@ -68,10 +79,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 //get suggestions based on newQuery
 
                 //pass them on to the search view
-                //mSearchView.swapSuggestions();
+                //mSearchView.swapSuggestions(mSearchSuggestion);
             }
         });
 
+
+
+
+        //listToDisplay.add(new CarPark())
 
         APIRetrieveSystem.retrieveCarParks(this);
 
@@ -143,25 +158,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setPadding(0, pxToDP(70), 0, pxToDP(70));
 
         mMap.setOnMyLocationButtonClickListener(onMyLocationButtonClickListener);
-        mMap.setOnMyLocationClickListener(onMyLocationClickListener);
         enableMyLocationIfPermitted();
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-
-        ArrayList<LatLng> carparks = new ArrayList<>(0);
-
         LatLng singapore = new LatLng(1.3521, 103.8198);
 
-        for(int i = 0; i < carparks.size(); i++){
-            mMap.addMarker(new MarkerOptions().position(carparks.get(i)).title("Marker No. " + i));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(singapore));
+        for(int i = 0; i < listToDisplay.size(); i++){
+            Marker mMarker;
+            //mMarker = mMap.addMarker(new MarkerOptions().position(listToDisplay.get(i).getLocation()).title(listToDisplay.get(i).carpark_number));
+            //mMarker.setTag(listToDisplay.get(i));
         }
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        // Set a listener for marker click.
+        mMap.setOnMarkerClickListener(this);
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(singapore,10));
+    }
+
+    /** Called when the user clicks a marker. */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Retrieve the data from the marker.
+        CarPark cp = (CarPark) marker.getTag();
+
+
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
     }
 
     private void enableMyLocationIfPermitted() {
@@ -210,24 +237,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             };
 
-    private GoogleMap.OnMyLocationClickListener onMyLocationClickListener =
-            new GoogleMap.OnMyLocationClickListener() {
-                @Override
-                public void onMyLocationClick(@NonNull Location location) {
-
-                    //mMap.setMinZoomPreference(12);
-
-                    CircleOptions circleOptions = new CircleOptions();
-                    circleOptions.center(new LatLng(location.getLatitude(),
-                            location.getLongitude()));
-
-                    circleOptions.radius(200);
-                    circleOptions.fillColor(Color.RED);
-                    circleOptions.strokeWidth(6);
-
-                    mMap.addCircle(circleOptions);
-                }
-            };
+//    private GoogleMap.OnMyLocationClickListener onMyLocationClickListener =
+//            new GoogleMap.OnMyLocationClickListener() {
+//                @Override
+//                public void onMyLocationClick(@NonNull Location location) {
+//
+//                    //mMap.setMinZoomPreference(12);
+//
+//                    CircleOptions circleOptions = new CircleOptions();
+//                    circleOptions.center(new LatLng(location.getLatitude(),
+//                            location.getLongitude()));
+//
+//                    circleOptions.radius(200);
+//                    circleOptions.fillColor(Color.RED);
+//                    circleOptions.strokeWidth(6);
+//
+//                    mMap.addCircle(circleOptions);
+//                }
+//            };
 
     private int pxToDP(int px){
         final float scale = findViewById(R.id.main_content).getContext().getResources().getDisplayMetrics().density;
---
+++ app/src/main/java/com/g2/androidapp/lotsoflots/PinViewDialogFragment.java
@@ -0,0 +1,30 @@
+package com.g2.androidapp.lotsoflots;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+public class PinViewDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.confirm_selection)
+                .setPositiveButton(R.string.confirm_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.confirm_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+}
