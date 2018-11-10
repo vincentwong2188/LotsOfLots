cf31d41bfd6969a5ee8452d85c6a688a1b347649
Vincent Wong <vincentwong.yong.sheng.2011@vjc.sg>
2018-11-11 04:05:54 +0800

    master sorting algorithm

--- app/src/main/java/com/g2/androidapp/lotsoflots/SortingSystem.java
+++ app/src/main/java/com/g2/androidapp/lotsoflots/SortingSystem.java
@@ -1,3 +1,17 @@
+/**
+ SortingSystem.java
+
+ Sorts CarParks based on:
+
+ (1) Distance:
+    sortCarParkbyDistance(LatLng llc)
+
+ (2) Vacancy:
+    sortCarParkbyVacancy(LatLng llc)
+
+ Written by Vincent Wong
+ */
+
 package com.g2.androidapp.lotsoflots;
 
 import android.util.Log;
@@ -8,118 +22,169 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class SortingSystem {
 
+    public static ArrayList<CarPark> sortCarParks(LatLng llc) {
+
+        ArrayList<CarPark> sortedCarparks = new ArrayList<CarPark>();
+
+        if (Preference.getSort() == "Distance") {
+
+            sortedCarparks = sortCarParkbyDistance(llc);
+        }
+        else{
+
+            sortedCarparks = sortCarParkbyVacancy(llc);
+        }
+
+        return sortedCarparks;
+
+    }
+
     /** To sort Car Park List according to Proximity to User */
 
-    public static ArrayList<CarPark> sortCarParkbyDistance(LatLng llc){
+    public static ArrayList<CarPark> sortCarParkbyDistance(LatLng llc) {
 
         /** Code first takes into account the user's filter of distance */
-
-
         //double userDistance = Preference.getDistance();
         double userDistance = 1000;
 
+        /** Code then takes into account the user's filter of vacancies */
         //int userVacancy = Preference.getVacancy();
-        int userVacancy = 100;
+        int userVacancy = 10;
 
+        /** Instantiation of CarPark ArrayLists to be used in the sorting algorithm */
         ArrayList<CarPark> temp = CarParkList.getCarParkList();
         ArrayList<CarPark> confirmed1 = new ArrayList<>();
         ArrayList<CarPark> confirmed2 = new ArrayList<>();
+        ArrayList<CarPark> confirmed3 = new ArrayList<>();
 
 
+        /** (1) Filtering based on Distance Filter */
+        for (int k = 0; k < temp.size(); k++) {
 
-        for (int k=0; k < temp.size(); k++){
-
-            if (temp.get(k).calcDistance(llc) <= userDistance){
+            if (temp.get(k).calcDistance(llc) <= userDistance) {
                 confirmed1.add(temp.get(k));
             }
         }
 
+        /** (2) Filtering results from (1) based on Vacancy Filter */
 
-        for (int n=0; n < confirmed1.size(); n++){
+        for (int n = 0; n < confirmed1.size(); n++) {
 
-            if (confirmed1.get(n).vacancies >= userVacancy){
+            if (confirmed1.get(n).vacancies >= userVacancy) {
                 confirmed2.add(confirmed1.get(n));
             }
         }
 
+        /** Code then sorts filtered results based on distance in ascending order */
+        for (int i = 1; i < confirmed2.size(); i++) {
 
-        for (int i=1; i< confirmed2.size(); i++){
+            for (int j = i; j > 0; j--) {
 
-            for (int j=i ; j>0; j--){
+                if (confirmed2.get(j).calcDistance(llc) < confirmed2.get(j - 1).calcDistance(llc)) {
 
-                if (confirmed2.get(j).calcDistance(llc) < confirmed2.get(j-1).calcDistance(llc)){
-
-                    CarPark temp2 = confirmed2.get(j-1);
-                    confirmed2.set(j-1,confirmed2.get(j));
-                    confirmed2.set(j,temp2);
+                    CarPark temp2 = confirmed2.get(j - 1);
+                    confirmed2.set(j - 1, confirmed2.get(j));
+                    confirmed2.set(j, temp2);
                 }
             }
         }
 
-
-        for(int m = 0; m < confirmed2.size(); m++) {
-            Log.d("Response", "sorted carparks by distance is: " + confirmed2.get(m).calcDistance(llc));
-            Log.d("Response", "carpark vacancy is: " + confirmed2.get(m).vacancies);
-            Log.d("Response", "sorted carparks by distance is: " + confirmed2.get(m).carpark_address);
+        /** Truncating array size to 10 elements or less */
+        if (confirmed2.size() > 10) {
+            for (int i = 0; i < 10; i++) {
+                confirmed3.add(confirmed2.get(i));
+            }
+        } else {
+            for (int i = 0; i < confirmed2.size(); i++) {
+                confirmed3.add(confirmed2.get(i));
+            }
         }
 
 
-        return confirmed2;
+        /** Printing elements in the array in Logcat */
+        for (int m = 0; m < confirmed3.size(); m++) {
+            Log.d("Response", "sorted carparks by distance is: " + confirmed3.get(m).calcDistance(llc));
+            Log.d("Response", "carpark vacancy is: " + confirmed3.get(m).vacancies);
+            Log.d("Response", "sorted carparks by distance is: " + confirmed3.get(m).carpark_address);
+        }
+
+
+        /** Return Value */
+        return confirmed3;
     }
 
-    /** To sort Car Park List according to Vacancy Count */
 
-    public static ArrayList<CarPark> sortCarParkbyVacancy(LatLng llc){
+    /**
+     * To sort Car Park List according to Vacancy Count
+     */
+
+    public static ArrayList<CarPark> sortCarParkbyVacancy(LatLng llc) {
 
         /** Code first takes into account the user's filter of distance */
+        //double userDistance = Preference.getDistance();
+        double userDistance = 1000;
 
-        double userDistance = Preference.getDistance();
-        //double userDistance = 1000;
-        int userVacancy = Preference.getVacancy();
-        //int userVacancy = 200;
+        /** Code then takes into account the user's filter of vacancies */
+        //int userVacancy = Preference.getVacancy();
+        int userVacancy = 200;
 
+        /** Instantiation of CarPark ArrayLists to be used in the sorting algorithm */
         ArrayList<CarPark> temp = CarParkList.getCarParkList();
         ArrayList<CarPark> confirmed1 = new ArrayList<>();
         ArrayList<CarPark> confirmed2 = new ArrayList<>();
+        ArrayList<CarPark> confirmed3 = new ArrayList<>();
 
 
-        /** Filters out carparks based on distance filter*/
-        for (int k=0; k < temp.size(); k++ ){
+        /** (1) Filtering based on Distance Filter */
+        for (int k = 0; k < temp.size(); k++) {
 
-            if (temp.get(k).calcDistance(llc) <= userDistance){
+            if (temp.get(k).calcDistance(llc) <= userDistance) {
                 confirmed1.add(temp.get(k));
             }
         }
 
 
-        /** Filters out carparks based on vacancy filter*/
-        for (int n=0; n < confirmed1.size(); n++){
+        /** (2) Filtering results from (1) based on Vacancy Filter */
+        for (int n = 0; n < confirmed1.size(); n++) {
 
-            if (confirmed1.get(n).getVacancy() >= userVacancy){
+            if (confirmed1.get(n).getVacancy() >= userVacancy) {
                 confirmed2.add(confirmed1.get(n));
             }
         }
 
-        /** Code then sorts the filtered Car Park list according to vacancy count*/
+        /** Code then sorts filtered results based on vacancy in descending order */
 
-        for (int i=1; i< confirmed2.size(); i++){
+        for (int i = 1; i < confirmed2.size(); i++) {
 
-            for (int j=i ; j>0; j--){
+            for (int j = i; j > 0; j--) {
 
-                if (confirmed2.get(j).getVacancy() > confirmed2.get(j-1).getVacancy()){
+                if (confirmed2.get(j).getVacancy() > confirmed2.get(j - 1).getVacancy()) {
 
-                    CarPark temp2 = confirmed2.get(j-1);
-                    confirmed2.set(j-1,confirmed2.get(j));
-                    confirmed2.set(j,temp2);
+                    CarPark temp2 = confirmed2.get(j - 1);
+                    confirmed2.set(j - 1, confirmed2.get(j));
+                    confirmed2.set(j, temp2);
                 }
             }
         }
 
-        for(int m = 0; m < confirmed2.size(); m++) {
-            Log.d("Response", "sorted carparks by vacancy is: " + confirmed2.get(m).vacancies);
-            Log.d("Response", "sorted carparks by vacancy address is: " + confirmed2.get(m).carpark_address);
+        /** Truncating array size to 10 elements or less */
+        if (confirmed2.size() > 10) {
+            for (int i = 0; i < 10; i++) {
+                confirmed3.add(confirmed2.get(i));
+            }
+        } else {
+            for (int i = 0; i < confirmed2.size(); i++) {
+                confirmed3.add(confirmed2.get(i));
+            }
         }
-        return confirmed2;
+
+        for (int m = 0; m < confirmed3.size(); m++) {
+            Log.d("Response", "sorted carparks by distance is: " + confirmed3.get(m).calcDistance(llc));
+            Log.d("Response", "sorted carparks by vacancy is: " + confirmed3.get(m).vacancies);
+            Log.d("Response", "sorted carparks by vacancy address is: " + confirmed3.get(m).carpark_address);
+        }
+
+        return confirmed3;
 
     }
 
