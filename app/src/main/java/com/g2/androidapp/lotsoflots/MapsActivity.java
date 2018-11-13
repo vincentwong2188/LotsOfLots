279db4f8ae12c1cada832646fbbc0f0fbb015420
Vincent Wong <vincentwong.yong.sheng.2011@vjc.sg>
2018-11-14 00:49:35 +0800

    master sorting algorithm again

--- app/src/main/java/com/g2/androidapp/lotsoflots/MapsActivity.java
+++ app/src/main/java/com/g2/androidapp/lotsoflots/MapsActivity.java
@@ -115,12 +115,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
