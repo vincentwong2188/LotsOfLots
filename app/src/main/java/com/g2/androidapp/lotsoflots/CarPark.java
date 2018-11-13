eded0d59f1961b9c61e20b7160ab2f06ab5a093f
millasml <millasml@yahoo.com.sg>
2018-11-13 21:22:10 +0800

    changed dependency on coordinateconverter

--- app/src/main/java/com/g2/androidapp/lotsoflots/CarPark.java
+++ app/src/main/java/com/g2/androidapp/lotsoflots/CarPark.java
@@ -32,8 +32,9 @@ public class CarPark {
         carpark_address = address;
         x_coord = x;
         y_coord = y;
-        lat = CoordinateConverter.convert(y_coord, x_coord).getLatitude();
-        lng = CoordinateConverter.convert(y_coord, x_coord).getLongitude();
+        LatLonCoordinate latlng = SVY21.computeLatLon(y_coord, x_coord);
+        lat = latlng.getLatitude();
+        lng = latlng.getLongitude();
         vacancies = 0;
 
     }
@@ -42,8 +43,9 @@ public class CarPark {
         carpark_address = address;
         x_coord = x;
         y_coord = y;
-        lat = CoordinateConverter.convert(y_coord, x_coord).getLatitude();
-        lng = CoordinateConverter.convert(y_coord, x_coord).getLongitude();
+        LatLonCoordinate latlng = SVY21.computeLatLon(y_coord, x_coord);
+        lat = latlng.getLatitude();
+        lng = latlng.getLongitude();
         vacancies = vac;
     }
 
